df4b47ab88f2e9fc02f84e8d00bd5cafd6338091
Thiago Ribeiro <thiago@toribeiro.com>
2019-05-04 13:28:11 -0500

    Problem Palindrome Permutation and inclusion of O(n) analysis

---
+++ c++/strings/ctci1-4.cpp
@@ -0,0 +1,38 @@
+//
+//  ctci1-4.cpp
+//  c++
+//
+//  Created by Thiago Ribeiro on 5/4/19.
+//  Copyright © 2019 Thiago Ribeiro. All rights reserved.
+//
+
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+bool is_palindrome_perm(string str) {
+    unordered_map<char, int> h;
+    
+    for (char &c : str) {
+        if (c != ' ') {
+            h[tolower(c)]++;
+        }
+    }
+    
+    int numberOfOdd = 0;
+    for (int i = 0 ; i < str.length(); ++i) {
+        if (h[str[i]] % 2 != 0) {
+            numberOfOdd++;
+        }
+    }
+    
+    return numberOfOdd == 1 || !numberOfOdd;
+}
+
+int main() {
+    string str = "elloello";
+    cout << is_palindrome_perm(str) << endl;
+    
+    return 0;
+}
