109b98e28539e235dd681b77493582b9a618deb6
tolribeiro <thiago@toribeiro.com>
2019-07-17 22:42:16 -0500

    check if anagram using hashtables

---
+++ c++/strings/check_if_anagram.cpp
@@ -0,0 +1,9 @@
+//
+//  check_if_anagram.cpp
+//  c++
+//
+//  Created by Thiago Ribeiro on 7/17/19.
+//  Copyright © 2019 Thiago Ribeiro. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ c++/strings/find_duplicates.cpp
@@ -0,0 +1,9 @@
+//
+//  find_duplicates.cpp
+//  c++
+//
+//  Created by Thiago Ribeiro on 7/17/19.
+//  Copyright © 2019 Thiago Ribeiro. All rights reserved.
+//
+
+#include <stdio.h>
